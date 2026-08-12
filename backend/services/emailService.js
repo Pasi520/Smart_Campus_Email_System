62601019200dae6eaddd15e61018a2fff2fe7320
M.G.M.Pasindu Ranaweera <methsara520@gmail.com>
2026-08-13 00:59:18 +0530

    Update emailService.js

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,1 +1,15 @@
+require('dotenv').config();
 
+const generateEmail = (studentNo) => {
+    const domain = process.env.EMAIL_DOMAIN;
+
+    if(!studentNo){
+        throw new Error ('Student number is required');
+    }
+
+    return `${studentNo.toLowerCase()}@${domain}`;
+};
+
+module.exports = {
+    generateEmail
+};
