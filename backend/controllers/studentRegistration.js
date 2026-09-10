20fcfe2349ff02fe0a2efe4b90a48b63e7b664f4
M.G.M.Pasindu Ranaweera <methsara520@gmail.com>
2026-09-10 23:18:23 +0530

    Update studentRegistration.js

--- backend/controllers/studentRegistration.js
+++ backend/controllers/studentRegistration.js
@@ -59,6 +59,74 @@ const register = async(req,res) => {
 
 };
 
+// get students rows 
+const getStudents = async (req,res) => {
+    try{
+        const [rows] = await db.query(
+            `SELECT * FROM students`
+        );
+
+        res.status(200).json({
+            success: true,
+            students: rows
+        });
+    }
+
+    catch (error) {
+        console.error('Error fetching students:', error);
+
+        res.status(500).json({
+            success:false,
+            message: 'Failed to fetch students'
+        });
+    }
+};
+
+// get one student row
+const getStudentId = async(req, res) =>{
+    try {
+
+        const studentId = Number(req.params.id);
+
+        // validate studend ID
+        if( !Number.isInteger(studentId) || studentId <= 0){
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid student ID'
+            });
+        }
+
+        // select studentId
+        const[rows] = await db.query(
+            `SELECT * FROM students WHERE student_id = ?`,
+            [studentId]
+        );
+
+        // student not found
+        if(rows.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: 'Student not found'
+            });
+        }
+
+        // return student
+        res.status(200).json({
+            success: true,
+            student:rows[0]
+           });
+    } catch(error){
+        console.error('Error fetching student:', error);
+
+        res.status(500).json({
+            success: false,
+            message:'Faild to fetch '
+        });
+    }
+};
+
 module.exports = {
-    register
+    register,
+    getStudents,
+    getStudentId 
 };
